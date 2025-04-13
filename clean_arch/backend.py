019bfd4ae4abf77a43403bdd34e0e903e99e34e7
HubertNguyen324 <47075978+HubertNguyen324@users.noreply.github.com>
2025-04-14 00:44:04 +0700

    fix clean architecture fastapi driver depends injection mechanism

--- clean_arch/backend.py
+++ clean_arch/backend.py
@@ -1,10 +1,22 @@
 
+from typing import Annotated
+
 from fastapi import Depends, FastAPI, HTTPException
 
 from clean_arch.interfaces import UserRepository
 from clean_arch.mem_repository import get_repository
 from clean_arch.schemas import UserCreate
 from clean_arch.use_cases import UserService
+
+# Define Dependency Injection
+
+
+def get_user_service(repo: UserRepository = Depends(get_repository)):
+    return UserService(repo)
+
+
+UserServiceDep = Annotated[UserService, Depends(get_user_service)]
+
 
 # Web Framework as an adapter (FastAPI)
 app = FastAPI()
@@ -13,15 +25,13 @@ app = FastAPI()
 
 
 @app.post("/user")
-def create_user(user: UserCreate, repo: UserRepository = Depends(get_repository)):
-    service = UserService(repo)
+def create_user(user: UserCreate, service: UserServiceDep):
     created_user = service.create_user(user.id, user.name)
     return created_user
 
 
 @app.get("/user/{user_id}")
-def read_user(user_id: str, repo: UserRepository = Depends(get_repository)):
-    service = UserService(repo)
+def read_user(user_id: str, service: UserServiceDep):
     user = service.find_user(user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
